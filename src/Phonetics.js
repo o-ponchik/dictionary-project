e90e3f31839f802389ba1e1492c744b54a071a31
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-10-06 18:39:41 +0300

    added_button_for_pronunciation_of_the_word

---
+++ src/Phonetics.js
@@ -0,0 +1,32 @@
+import React from "react";
+import useSound from "use-sound";
+import { HiVolumeUp } from "react-icons/hi";
+
+export default function Phonetics(props) {
+  console.log(props.phonetic.audio);
+
+  const Button = () => {
+    const [play] = useSound(props.phonetic.audio);
+
+    if (props.phonetic.audio) {
+      return (
+        <HiVolumeUp
+          color="#0d7e83"
+          fontSize="1.5rem"
+          onClick={play}
+          cursor="pointer"
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div className="Phonetics">
+      <Button />
+    </div>
+  );
+}
+
+// BsVolumeDownFill;
--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,38 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-  console.log(props.results);
+  const PhoneticLetter = () => {
+    if (props.results.phonetic) {
+      return <p>{props.results.phonetic} </p>;
+    } else {
+      return <p>No phonetic was found 🙁</p>;
+    }
+  };
 
   if (props.results) {
+    console.log(props.results.phonetics[0].audio);
     return (
       <div className="container">
         <div className="content">
-          <div className="searching-word text-center">
+          <div className="searching-word mb-5">
             <h2>{props.results.word}</h2>
-            <p>{props.results.phonetic}</p>
+            <div className="row mt-3">
+              <div className="col-auto">
+                <PhoneticLetter />
+              </div>
+              <div className="col-auto text-start">
+                {props.results.phonetics.map(function (phonetic, index) {
+                  return (
+                    <span className="pe-3">
+                      <Phonetics key={index} phonetic={phonetic} />
+                    </span>
+                  );
+                })}
+                {/* <Phonetics phonetic={props.results.phonetics[0]} /> */}
+              </div>
+            </div>
           </div>
 
           {/* meaning */}
